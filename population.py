de407c14d32e2028063a73e5f52edf3c9427c6f2
Mohammad Hayajneh <35841393+mohammadhayajneh@users.noreply.github.com>
2018-02-14 12:41:29 -0500

    Add files via upload

---
+++ population.py
@@ -0,0 +1,16 @@
+population=int(input('Enter Population:'))
+land_area=float(input('Enter Land Area:'))
+if population < 10000000:
+    print (population)
+
+if 10000000<population<35000000:
+    print (population)
+
+if population/land_area>100:
+    print ('Densely populated')
+
+if population/land_area>100:
+    print ('Densely populated')
+
+else:
+    print ('Sparsely populated')
